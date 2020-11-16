e4cd432501dde4327e53de1ea892a50da78dd709
ITRussell <IanThomasR@gmail.com>
2020-11-16 22:18:33 +0000

    Practice

--- Grokking/All/practice.cpp
+++ Grokking/All/practice.cpp
@@ -1,13 +1,12 @@
 /*
-Given a binary tree, populate an array to represent its level-by-level traversal. 
-You should populate the values of all nodes of each level from left to right in separate sub-arrays.
+Given a binary tree, populate an array to represent the averages of all of its levels.
 */
 
 using namespace std;
-
+#include<iostream>
 #include<vector>
 #include<queue>
-#include<iostream>
+#include<deque>
 
 class TreeNode {
 public:
@@ -20,50 +19,24 @@ public:
     }
 };
 
-class Operations{
+class Operations {
 public:
-    static vector<vector<int>> traverse(TreeNode* root){
-       vector<vector<int>> result;
-       if (root == nullptr){
-           return result;
-       }     
-       queue<TreeNode*> q;
-       q.push(root);
-       while (!q.empty()){
-           int levelSize = q.size();
-           vector<int> currLevel;
-           for (int i = 0; i < levelSize; i++){
-               TreeNode* currNode = q.front();
-               q.pop();
-               currLevel.push_back(currNode->key);
-               if (currNode->left != nullptr){
-                   q.push(currNode->left);
-               }
-               if (currNode->right != nullptr){
-                   q.push(currNode->right);
-               }
-           }
-           result.push_back(currLevel);
-       }
-       return result;
-    }
+   
 };
 
-int main(){
-    TreeNode* root = new TreeNode(1);
-    root->left = new TreeNode(2);
-    root->right = new TreeNode(3);
-
-    root->left->right = new TreeNode(4);
-    root->right->right = new TreeNode(5);
-
-
-    vector<vector<int>> res = Operations::traverse(root);
-
-    for (auto level : res){
-        for (auto val : level){
-            cout << val << " ";
-        }
-        cout << "\n";
+int main(int argc, char *argv[]) {
+  TreeNode *root = new TreeNode(12);
+  root->left = new TreeNode(7);
+  root->right = new TreeNode(1);
+  root->left->left = new TreeNode(9);
+  root->right->left = new TreeNode(10);
+  root->right->right = new TreeNode(5);
+  deque<vector<int>> result;
+  cout << "Level order traversal: ";
+  for (auto vec : result) {
+    for (auto num : vec) {
+      cout << num << " ";
     }
+    cout << endl;
+  }
 }
